e82f2f8dfba2765663f88113de16122377632aed
mrpapercut <947626+mrpapercut@users.noreply.github.com>
2024-02-28 19:42:39 +0100

    Using proper logging

--- feeder/Scheduler.py
+++ feeder/Scheduler.py
@@ -1,11 +1,14 @@
 import schedule
 import threading
 import time
+import logging
 
 from datetime import datetime
 
 from .YoutubeChannels import YoutubeChannels
 from .SQLite import SQLite
+
+logging.basicConfig(level=logging.INFO)
 
 def ts():
     return f"[{datetime.now().strftime('%H:%M:%S')}]"
@@ -27,29 +30,32 @@ class Scheduler(threading.Thread):
         self.db.init_db()
 
     def run(self):
-        print(f"{ts()} Starting scheduler")
+        logging.info(f"{ts()} Starting scheduler")
 
         self.init_db()
 
         self.is_running = True
 
-        # schedule.every(1).minutes.do(self.test_scheduler)
+        schedule.every(1).minutes.do(self.test_scheduler)
         schedule.every(15).minutes.do(self.update_youtube_channels)
 
         while self.is_running:
             schedule.run_pending()
             time.sleep(1)
 
-        print(f"{ts()} Scheduler shutting down")
+        logging.info(f"{ts()} Scheduler shutting down")
         self.db.close()
 
     def stop(self):
         self.is_running = False
 
     def test_scheduler(self):
-        print(f"{ts()} Scheduler is running")
+        logging.info(f"{ts()} Scheduler is running")
 
     def update_youtube_channels(self):
-        print(f"{ts()} Updating Youtube channels")
+        logging.info(f"{ts()} Updating Youtube channels")
+
         yt = YoutubeChannels(self.config, self.db)
         yt.get_videos()
+
+        logging.info(f"{ts()} Finished updating Youtube channels")
