02e1f63a3bf790f8c37282788573faea443b3e92
mrpapercut <947626+mrpapercut@users.noreply.github.com>
2024-03-24 17:56:48 +0100

    Refactored so it can deal with files & dirs

--- nodejs/pngjs.js
+++ nodejs/pngjs.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
 const { PNG } = require('pngjs');
 
-const dirname = './mario-spritesheets/individual/mario-smw-walk/';
+// const locationname = './mario-spritesheets/individual/mario-smw-walk/';
+const locationname = './mario-spritesheets/individual/yt-logos/grandpoobear.png';
 
 const results = {
     width: 0,
@@ -11,60 +12,72 @@ const results = {
     num_sheets: 0,
     fps: 12,
     animation: [],
-    colors: [],
+    colors: [0],
     pixeldata: []
 }
 
-fs.readdir(dirname, (err, files) => {
-    if (err) return;
+async function processFiles() {
+    const stat = await fs.lstat(locationname)
+    const isDir = stat.isDirectory();
 
-    files.forEach(file => {
-        const filepath = path.join(dirname, file);
+    if (isDir) {
+        try {
+            const dirfiles = await fs.readdir(locationname);
 
-        const filecontents = fs.readFileSync(filepath);
+            for (let i = 0; i < dirfiles.length; i++) {
+                const filepath = path.join(locationname, dirfiles[i]);
 
-        const png = PNG.sync.read(filecontents);
+                const filecontents = await fs.readFile(filepath);
 
-        const sheetPixels = [];
-
-        if (png.width > results.width) results.width = png.width;
-        if (png.height > results.height) results.height = png.height;
-        results.num_sheets++;
-
-        for (let y = 0; y < png.height; y++) {
-            sheetPixels[y] = [];
-
-            for (let x = 0; x < png.width; x++) {
-                const index = (png.width * y + x) << 2;
-                const [red, green, blue, alpha] = new Array(4).fill(0).map((_, i) => png.data[index + i]);
-
-                if (alpha === 0) {
-                    sheetPixels[y][x] = -1;
-                } else {
-                    const hexColor = eval(`0x${[red, green, blue].map(c => c.toString(16).padStart(2, '0')).join('')}`);
-
-                    if (!results.colors.includes(hexColor)) results.colors.push(hexColor);
-
-                    sheetPixels[y][x] = results.colors.indexOf(hexColor);
-                }
+                handleFile(filecontents)
             }
+        } catch (err) {
+            return;
         }
+    } else {
+        results.fps = 1;
 
-        results.pixeldata.push(sheetPixels);
-    });
+        const filecontents = await fs.readFile(locationname);
+
+        handleFile(filecontents);
+    }
 
     results.animation = new Array(results.pixeldata.length).fill().map((_, i) => i);
 
     console.log(JSON.stringify(results));
-});
+}
 
-function convertObjToCPP(spritesheet) {
-    const { width, height, numSheets, colors, pixelData } = spritesheet;
-    const sheets = [];
+(async () => {
+    processFiles()
+})();
 
-    pixelData.forEach((sheet, idx) => {
-        sheets[idx] = `{${sheet.map(row => `{${row.join(', ')}}`).join(',\n')}}`;
-    });
+function handleFile(filecontents) {
+    const png = PNG.sync.read(filecontents);
 
-    return `{\n${width}, ${height}, ${numSheets},\n{${colors.map(c => `0x${c.toString(16).padStart(6, '0')}`).join(', ')}},\n{${sheets.join(',\n')}}}`;
+    const sheetPixels = [];
+
+    if (png.width > results.width) results.width = png.width;
+    if (png.height > results.height) results.height = png.height;
+    results.num_sheets++;
+
+    for (let y = 0; y < png.height; y++) {
+        sheetPixels[y] = [];
+
+        for (let x = 0; x < png.width; x++) {
+            const index = (png.width * y + x) << 2;
+            const [red, green, blue, alpha] = new Array(4).fill(0).map((_, i) => png.data[index + i]);
+
+            if (alpha === 0) {
+                sheetPixels[y][x] = -1;
+            } else {
+                const hexColor = eval(`0x${[red, green, blue].map(c => c.toString(16).padStart(2, '0')).join('')}`);
+
+                if (!results.colors.includes(hexColor)) results.colors.push(hexColor);
+
+                sheetPixels[y][x] = results.colors.indexOf(hexColor);
+            }
+        }
+    }
+
+    results.pixeldata.push(sheetPixels);
 }
